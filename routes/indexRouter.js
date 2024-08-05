c2f22ef1e07a12086ca50ecf3019bf98a50c81ea
Sova <108209320+NceSova@users.noreply.github.com>
2024-08-05 14:32:36 +0300

    feat: add message detail page

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,24 +1,29 @@
 const {Router} = require("express");
+const {v4: uuidv4} = require("uuid");
 
 const indexRouter = Router();
 
 const messages = [
   {
+    id: uuidv4(),
     text: "Hi there!",
     user: "Amando",
     added: new Date(),
   },
   {
+    id: uuidv4(),
     text: "Hello World!",
     user: "Charles",
     added: new Date(),
   },
   {
+    id: uuidv4(),
     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam est nibh, mollis vel dui quis, ullamcorper dignissim magna. Quisque faucibus.",
     user: "Troll",
     added: new Date(),
   },
   {
+    id: uuidv4(),
     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi convallis nec massa vitae mollis. Aliquam nec massa ac elit condimentum sodales eu quis mauris. Vivamus sodales pellentesque lectus ac luctus. In hac habitasse platea dictumst. In metus felis, aliquam ac mollis non, imperdiet id metus. Proin ligula lectus, congue id.",
     user: "exmaple",
     added: new Date(),
@@ -36,8 +41,19 @@ indexRouter.get("/new", (req, res) => {
 indexRouter.post("/new", (req, res) => {
   const messageName = req.body.name;
   const messageText = req.body.message;
-  messages.push({text: messageText, user: messageName, added: new Date()});
+  messages.push({
+    id: uuidv4(),
+    text: messageText,
+    user: messageName,
+    added: new Date(),
+  });
   res.redirect("/");
+});
+
+indexRouter.get("/message/:id", (req, res) => {
+  res.render("descMessage", {
+    message: messages.filter((item) => item.id === req.params.id)[0],
+  });
 });
 
 module.exports = indexRouter;
